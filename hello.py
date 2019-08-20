3cd66f8e476c2dfe362a31747e571f4ae3e27b0b
Gehwan24 <53293830+Gehwan24@users.noreply.github.com>
2019-08-20 14:49:31 +0900

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+from Cryptodome.Cipher import AES
+import firebase
+from firebase import firebase
+import os.path
+import struct
+import hashlib
+import ctypes
+import json
+SALT_SIZE = 16  # This size is arbitrary
+IV_SIZE = 16    # 128 bit, fixed for the AES algorithm
+KEY_SIZE = 32   # 256 bit meaning AES-256, can also be 128 or 192 bits
+
+
+password = 'hwanlee'
+password = password.encode()
+
+salt = os.urandom(SALT_SIZE)
+testsalt = b'0912'
+base = hashlib.pbkdf2_hmac('sha256', password, testsalt, 100000, dklen=IV_SIZE + KEY_SIZE)
+iv = base[0:IV_SIZE]
+key = base[IV_SIZE:]
+
+firebase = firebase.FirebaseApplication('https://keydata-e5fb1.firebaseio.com/', None)
+
+result = firebase.post('/user',{'ID':str(password),'IV':str(iv),'KEY':str(key)})
+
+result2 = firebase.get('/user',None)
+print(result2)
