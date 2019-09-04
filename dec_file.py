9e7b0eb3ed7f6a495a94627ee1efc053be867d28
hwanlee12 <tmdghks912@naver.com>
2019-09-04 22:08:10 +0900

    firebase decrypt

--- dec_file.py
+++ dec_file.py
@@ -1,7 +1,8 @@
 from Cryptodome.Cipher import AES
 import os.path
 import struct
-import hashlib
+from uuid import getnode as get_mac
+import firebase
 
 IV_SIZE = 16    # 128 bit, fixed for the AES algorithm
 KEY_SIZE = 32   # 256 bit meaning AES-256, can also be 128 or 192 bits
@@ -47,12 +48,16 @@ testPath = 'C:/Users/tmdgh/Desktop/test123/'
 
 password = 'hwanlee'
 password = password.encode()
-
-salt = os.urandom(SALT_SIZE)
 testsalt = b'0912'
-base = hashlib.pbkdf2_hmac('sha256', password, testsalt, 100000, dklen=IV_SIZE + KEY_SIZE)
-iv = base[0:IV_SIZE]
-key = base[IV_SIZE:]
+
+mac = get_mac()
+password = str(mac)
+
+result2 = firebase.get('/user', )
+
+password = password.encode()
+
+
 
 print("decoding")
 search_dec(testPath)
